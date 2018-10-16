38076c36241d6305041148c11bf9d0dbe46aeb7f
Daybeling Lugo Blanco <daybeling.lugo15@est.unanleon.edu.ni>
2018-10-16 14:47:00 -0600

    Ajustes formulario

---
+++ app.js
@@ -0,0 +1,19 @@
+var formulario = document.getElementById("id_formulario"),
+    comodin = true;
+ 
+formulario.addEventListener("submit", function(event){
+    event.preventDefault();
+ 
+    var elementos = this.elements;
+    for (var i in elementos){
+        if (!elementos[i].value.length){
+            alert("Debe de completar el campo " + elementos[i].name);
+            comodin = false;
+            break;
+        }
+    }
+ 
+    if (comodin){
+        this.submit();
+    }
+}, false);
