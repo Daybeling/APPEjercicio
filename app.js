b17df3f0bb5b8a4a57edbc09cd72a9597b75d8f8
Daybeling Lugo Blanco <daybeling.lugo15@est.unanleon.edu.ni>
2018-10-22 16:13:11 -0600

    Editando función

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-var form = document.getElementById("id_formulario");
-
-form.addEventListener("submit", function(event){
-  event.preventDefault();
-  var comodin = true,
-  elementos = this.elements,
-  total = elementos.length;
-  
-  for (var i = 0; i < total; i++){
-    if (!elementos[i].value.length){
-      alert ("Debes de ingresar el " + elementos[i].name);
-      elementos[i].focus();
-      comodin = false;
-      break;
+window.addEventListener('load',cargar);
+function cargar(){
+  var form = document.getElementById("id_formulario");
+  form.addEventListener("submit", function(event){
+    event.preventDefault();
+    var comodin = true,
+    elementos = this.elements,
+    total = elementos.length;
+    
+    for (var i = 0; i < total; i++){
+      if (!elementos[i].value.length){
+        alert ("Debes de ingresar el " + elementos[i].name);
+        elementos[i].focus();
+        comodin = false;
+        break;
+      }
     }
-  }
-  
-  if (comodin){
-    this.submit();
-  }
-}, false);
+    
+    if (comodin){
+      this.submit();
+    }
+  }, false); 
+}
