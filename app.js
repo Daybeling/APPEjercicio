dc11fc403142487c8d2db30200f35d8e92311eff
Daybeling Lugo Blanco <daybeling.lugo15@est.unanleon.edu.ni>
2018-10-22 09:12:01 -0600

    editando JS

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-var formulario = document.getElementById("id_formulario"),
-    comodin = true;
+// var formulario = document.getElementById("id_formulario"),
+//     comodin = true;
  
-formulario.addEventListener("submit", function(event){
-    event.preventDefault();
+// formulario.addEventListener("submit", function(event){
+//     event.preventDefault();
  
-    var elementos = this.elements;
-    for (var i in elementos){
-        if (!elementos[i].value.length){
-            alert("Debe de completar el campo " + elementos[i].name);
-            comodin = false;
-            break;
-        }
-    }
+//     var elementos = this.elements;
+//     for (var i in elementos){
+//         if (!elementos[i].value.length){
+//             alert("Debe de completar el campo " + elementos[i].name);
+//             comodin = false;
+//             break;
+//         }
+//     }
  
-    if (comodin){
-        this.submit();
-    }
-}, false);
+//     if (comodin){
+//         this.submit();
+//     }
+// }, false);
+
